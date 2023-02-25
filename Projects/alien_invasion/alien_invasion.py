38b30d8d0e55d57a92f043cbe5f5bf5a72402ff0
Pravesh Aryal <pravesh.aryal696969@gmail.com>
2023-02-25 11:59:31 +0545

    refactor game

--- Projects/alien_invasion/alien_invasion.py
+++ Projects/alien_invasion/alien_invasion.py
@@ -1,4 +1,31 @@
-def moo():
-    print("moo")
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+import game_funtions as gf
 
-moo()
+# page : 280
+def run_game():
+
+    # create a screen object
+    pygame.init()
+    # alien invasion settings; create an instance of the class Settings
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height)
+    )
+
+    pygame.display.set_caption("Alien Invasion")
+
+    # make a ship
+    ship = Ship(screen)
+
+    # main loop for the game
+    while True:
+
+        # watching for keyboard and mouse events
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
+
+
+run_game()
---
+++ Projects/alien_invasion/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class to store all the settings for the game"""
+
+    def __init__(self):
+        """Initialize the game settings"""
+        # screen settings
+        self.screen_width = 600
+        self.screen_height = 400
+        self.bg_color = (230, 230, 230)
